8568dbd0a1abaec33b3402cc224eaf7f9235111c
Adamster8888 <adamster8888@gmail.com>
2024-11-02 22:20:41 -0700

    worked on parseIfAndWhile. mostly done, relys on helper functions parseExpression and parseStatementList, which don't exist.

--- Interpreter/AbstractSyntaxTree.h
+++ Interpreter/AbstractSyntaxTree.h
@@ -26,6 +26,62 @@ public:
     //parseExpression
     //
     //
+
+    void parseIfAndWhile() {
+        CST->resetCurrentNode();
+        Node* current = CST->getCurrentNode();
+        if (!current) return;
+
+        if (current->data.type == "If") {
+            // Create the AST node for "If"
+            Node* ifNode = new Node(Token("If"));
+            AST->insertChild(ifNode);  // Add If node to the AST
+            AST->resetCurrentNode();   // Move currentNode to the newly added If node
+
+            // Move to the condition expression
+            CST->nextNode();
+            Node* conditionNode = CST->getCurrentNode();
+            if (conditionNode) {
+                Node* conditionAST = parseExpression(conditionNode);
+                AST->insertChild(conditionAST);  // Attach condition as child of If node
+            }
+
+            // Move to the body of the If statement
+            CST->nextNode();
+            Node* ifBodyNode = CST->getCurrentNode();
+            if (ifBodyNode) {
+                Node* ifBodyAST = parseStatementList(ifBodyNode);
+                AST->insertSibling(ifBodyAST);  // Attach body as sibling of the condition node
+            }
+        } 
+        else if (current->data.type == "While") {
+            // Create the AST node for "While"
+            Node* whileNode = new Node(Token("While"));
+            AST->insertChild(whileNode);  // Add While node to the AST
+            AST->resetCurrentNode();      // Move currentNode to the newly added While node
+
+            // Move to the condition expression
+            CST->nextNode();
+            Node* conditionNode = CST->getCurrentNode();
+            if (conditionNode) {
+                Node* conditionAST = parseExpression(conditionNode);
+                AST->insertChild(conditionAST);  // Attach condition as child of While node
+            }
+
+            // Move to the body of the While loop
+            CST->nextNode();
+            Node* whileBodyNode = CST->getCurrentNode();
+            if (whileBodyNode) {
+                Node* whileBodyAST = parseStatementList(whileBodyNode);
+                AST->insertSibling(whileBodyAST);  // Attach body as sibling of the condition node
+            }
+        }
+    }
+
+
+
+
+
 };
 
 
